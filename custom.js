d0930c15de87caa916abe33b691f83095d0e6c49
Firdaus Abd Rahman <firdaus.rahman.777@gmail.com>
2019-04-08 00:40:29 +0800

    first commit

---
+++ custom.js
@@ -0,0 +1,25 @@
+//get input element
+let filterInput = document.getElementById("search-txt");
+//add event listener
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames(){
+  //get value of input
+  let filterValue = document.getElementById("search-txt").value.toUpperCase();
+
+  //get names ul
+  let ul = document.getElementById("names");
+  //get Lis from ul
+  let li = ul.querySelectorAll("li.collection-item");
+
+  //loop through collection item Lis
+  for(let i=0; i <li.length; i++){
+    let a = li[i].getElementsByTagName("a")[0];
+    //if matched
+    if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+      li[i].style.display = "";
+    }else{
+      li[i].style.display = "none";
+    }
+  }
+}
